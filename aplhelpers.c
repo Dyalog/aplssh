7e3f8234545d1cf56110e4333036db2b79d9791c
Marinus <marinuso@gmail.com>
2017-08-18 09:37:38 +0100

    support building as dll on windows

--- aplhelpers.c
+++ aplhelpers.c
@@ -2,13 +2,13 @@
    The structs differ per platform and there's no way to see that from APL.
 */
 
-#include <stdint.h>
-#include <libssh2.h>
 
-int8_t test() { return 42; }
+#include "aplhelpers.h"
 
-uint64_t stat_size(libssh2_struct_stat *s) { return s->st_size; }
-int32_t stat_mode(libssh2_struct_stat *s) { return s->st_mode; }
-int64_t stat_atime(libssh2_struct_stat *s) { return s->st_atime; }
-int64_t stat_mtime(libssh2_struct_stat *s) { return s->st_mtime; }
+int8_t   ADDCALL test() { return 42; }
+
+uint64_t ADDCALL stat_size(libssh2_struct_stat *s) { return s->st_size; }
+int32_t  ADDCALL stat_mode(libssh2_struct_stat *s) { return s->st_mode; }
+int64_t  ADDCALL stat_atime(libssh2_struct_stat *s) { return s->st_atime; }
+int64_t  ADDCALL stat_mtime(libssh2_struct_stat *s) { return s->st_mtime; }
 
